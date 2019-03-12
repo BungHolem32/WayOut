061227d457c621c29b30b611436e977137b8b5c2
Ilan Vachtel <ilanvac@gamil.com>
2019-03-12 11:38:56 +0200

    update docs

---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>WayOUT</span>
+        <span class="font-weight-light"> HOME ASSIGNMENT</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ client/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
+import router from './routes';
+import store from './store';
+import VeeValidate from 'vee-validate'
+
+require('dotenv').config();
+
+Vue.use(VeeValidate);
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app');
